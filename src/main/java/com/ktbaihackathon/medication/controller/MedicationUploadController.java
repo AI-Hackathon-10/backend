b16d555be89f240cb57064684c3eb495e54cbada
hyo-lin <lin1137001@gmail.com>
2026-08-19 19:05:18 +0900

    feat: 약 사진 업로드 controller

---
+++ src/main/java/com/ktbaihackathon/medication/controller/MedicationUploadController.java
@@ -0,0 +1,36 @@
+package com.ktbaihackathon.medication.controller;
+
+import com.ktbaihackathon.common.response.ApiResponse;
+import com.ktbaihackathon.common.response.ResultCode;
+import com.ktbaihackathon.medication.dto.PillUploadUrlsResponse;
+import com.ktbaihackathon.medication.dto.PresignUrlRequest;
+import com.ktbaihackathon.medication.service.S3PresignService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/medications/upload")
+@RequiredArgsConstructor
+public class MedicationUploadController {
+
+    private final S3PresignService s3PresignService;
+
+    @PostMapping("/presigned-url")
+    public ResponseEntity<ApiResponse<PillUploadUrlsResponse>> getPresignedUrls(
+            @RequestBody PresignUrlRequest request) {
+
+        PillUploadUrlsResponse result = s3PresignService.generateUploadUrls(request.sessionId());
+
+        return ResponseEntity.ok(
+                ApiResponse.success(
+                        ResultCode.PRESIGNED_URL_ISSUED.name(),
+                        ResultCode.PRESIGNED_URL_ISSUED.getMessage(),
+                        result
+                )
+        );
+    }
+}
