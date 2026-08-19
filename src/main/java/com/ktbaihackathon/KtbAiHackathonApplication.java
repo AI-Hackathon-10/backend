572aa9530afce05bddfab5870166779a64568779
Megazizon <wnsdh8846@naver.com>
2026-08-19 14:29:28 +0900

    chore: initial commit

---
+++ src/main/java/com/ktbaihackathon/KtbAiHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.ktbaihackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtbAiHackathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KtbAiHackathonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ktbaihackathon/KtbAiHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktbaihackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtbAiHackathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
