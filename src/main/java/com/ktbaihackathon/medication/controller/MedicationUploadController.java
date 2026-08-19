a19eb0529d54d589ecd70dc03c872067220f355f
hyo-lin <lin1137001@gmail.com>
2026-08-19 20:30:39 +0900

    feat: import 문 추가

--- src/main/java/com/ktbaihackathon/medication/controller/MedicationUploadController.java
+++ src/main/java/com/ktbaihackathon/medication/controller/MedicationUploadController.java
@@ -7,10 +7,7 @@ import com.ktbaihackathon.medication.dto.PresignUrlRequest;
 import com.ktbaihackathon.medication.service.S3PresignService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/medications/upload")
