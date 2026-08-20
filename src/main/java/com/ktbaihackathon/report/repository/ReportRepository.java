2664e63ef1d2fb1938a88d3ed9611bf19d243b18
정연상 <jeong-yeonsang@jeong-yeonsangs-MacBook-Pro.local>
2026-08-20 10:49:10 +0900

    feat: 리포트 목록 조회 서비스 구현

--- src/main/java/com/ktbaihackathon/report/repository/ReportRepository.java
+++ src/main/java/com/ktbaihackathon/report/repository/ReportRepository.java
@@ -3,5 +3,9 @@ package com.ktbaihackathon.report.repository;
 import com.ktbaihackathon.report.entity.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReportRepository extends JpaRepository<Report, Long> {
+
+    List<Report> findAllByUser_UserIdOrderByCreatedAtDesc(Long userId);
 }
